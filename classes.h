27b564451f3099a4004d3fbdb7c148bc759f545d
Harlamis <egordan74@gmail.com>
2025-02-22 00:23:41 +0200

    fix

--- classes.h
+++ classes.h
@@ -5,14 +5,20 @@ class GameObject {
 private:
 	std::string Name;
 	int mass;
-	int posX;
-	int posY;
-	int posZ;
+	int position;
 public:
 	void Collide(GameObject& obj1, GameObject& obj2) {
-		if (obj1.posX == obj2.posX) {
-			
+		if (obj1.mass < obj2.mass) {
+			obj1.position -= (obj2.mass - obj1.mass);
+		}
+		else if (obj1.mass > obj2.mass) {
+			obj2.position -= (obj2.mass - obj1.mass);
+		}
+		else if (obj1.mass == obj2.mass) {
+			obj1.position--;
+			obj2.position--;
 		}
 	};
+
 
 };
